f86faef3766cdfd6027c413baa8c53a592b545cb
JeongNi <je_0922@naver.com>
2024-01-15 10:13:57 +0900

    Update face_emotion_classifier.py

--- face/face_emotion/face_emotion_classifier.py
+++ face/face_emotion/face_emotion_classifier.py
@@ -108,7 +108,7 @@ import os
 if __name__ == "__main__":
     emotions_per_frame, emotion_labels, clip_name = process()
 
-    output_path = f"D:/MultiModal/multi_modal/results/face/face_emotion/{clip_name.split('/')[5]}"
+    output_path = f"D:/MultiModal/MultiModal_Model/results/face/face_emotion/{clip_name.split('/')[5]}"
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
